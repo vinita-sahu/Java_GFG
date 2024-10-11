34e2703e28ca0f1ee7ef9e4615a5f4aed63a26a4
vinita-sahu <108357164+vinita-sahu@users.noreply.github.com>
2024-10-11 13:16:36 +0000

    ReorganizeTheArray

---
+++ ReorganizeTheArray/Solution.java
@@ -0,0 +1,11 @@
+//package ReorganizeTheArray;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> rearrange(List<Integer> arr) {
+        // Code here
+        List<Integer> result = ;
+        return result;
+    }
+}
